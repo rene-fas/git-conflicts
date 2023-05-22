07b6d12c2e4147b8f1e3f202f75586ebdc450897
René Faßbender <rene-fassb@gmx.de>
2023-05-22 11:23:41 +0200

    fix

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
 const dateElement = document.querySelector('[data-js="date"]');
 const date = new Date().toLocaleDateString("en-US");
 dateElement.textContent = date;
-
-document.querySelector('[data-js="year"]').textContent =
-  new Date().getFullYear();
